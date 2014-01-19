2be29e6b24c6d6f662783a592772490651aebe13
Manuel Gonzalez Garcia <leunam.san@gmail.com>
2014-01-19 23:52:34 +0100

    fancybox css edit and code refactor

--- radicalsite/radicalwebapp/ajax.py
+++ radicalsite/radicalwebapp/ajax.py
@@ -7,7 +7,6 @@ from dajaxice.decorators import dajaxice_register
 
 from radicalwebapp import views
 from django.conf import settings
-# from imagekit.cachefiles import ImageCacheFile
 
 @dajaxice_register
 def get_portfolio(request, page, tag):
@@ -24,11 +23,6 @@ def get_portfolio(request, page, tag):
 		pictures = Portfolio.objects.filter(tag=tag).all()
 		portfolio_length = Portfolio.objects.filter(tag=tag).count()
 	
-
-	print str(page) + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< PAGE >>>>>>>>>>>>>"
-	print str(portfolio_length) + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< pictures_length"
-	print str(ids_to_get_to) + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< ids_to_get_to"
-
 	html = ""
 	for pic in pictures[ids_to_get_from:ids_to_get_to]:
 
@@ -36,11 +30,9 @@ def get_portfolio(request, page, tag):
 		html += "	<div class='hover'>"
 		html += "		<h5>" + pic.platform + "</h5>"
 		html += "		<p>" + pic.name + "</p>"
-		html += "		<a href='" + pic.image.url + "' class='btn fancybox'><b class='icon-search'></b></a>"
+		html += "		<a href='" + pic.image.url + "' class='btn fancybox' title='"+ pic.name + " - " + pic.platform + "'><b class='icon-search'></b></a>"
 		html += "	</div>"
 		html += "</div>"
-
-
 
 	if (portfolio_length<ids_to_get_to):
 		ids_to_get_to = portfolio_length
@@ -48,10 +40,21 @@ def get_portfolio(request, page, tag):
 	html += "<input type='hidden' value='" + str(page) + "' id='next_page'>"
 	html += "<input type='hidden' value='" + str(ids_to_get_to) + "' id='last_id'>"
 	html += "<input type='hidden' value='" + str(portfolio_length) + "' id='portfolio_length'>"
-	html += "<script>eval($('.fancybox').fancybox({closeBtn  : true,}))</script>"
+	html += "<script>"
+	html += "	eval($('.fancybox')"
+	html += "		.fancybox({"
+	html += " 			padding : 0,"
+	html += " 			openEffect:'elastic',"
+	html += " 			closeBtn  : true,"
+	html += " 			helpers:{"
+	html += "				title:{"
+	html += "					type : 'over'"
+	html += "				}"
+	html += "			}"
+	html += "		})"
+	html += "	)"
+	html += "</script>"
 
 	dajax.assign('#portfolio_pics', 'innerHTML', html)
-	
-	
 
 	return dajax.json()
